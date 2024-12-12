bb6c31e023764bfdb21561b8733253aac2b6f202
Ubuntu <patil@gmail.com>
2024-12-12 07:36:59 +0000

    adding all file

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main.jsx'
+
+const App = () => {
+  return (
+    <>
+      <Sidebar/>
+      <Main/>
+
+    </>
+  )
+}
+
+export default App
